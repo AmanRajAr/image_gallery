e06282e6f71e7fac3894035bec59986d8e76905d
Aman <rajaman804784@gmail.com>
2022-06-13 18:19:01 +0530

    initial commit

---
+++ src/components/upload/Upload.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Form from './Form'
+import ProgressList from './progressList/ProgressList'
+
+
+const Upload = () => {
+  return (
+    <div>
+     <Form /> 
+     <ProgressList />
+    </div>
+  )
+}
+
+export default Upload
---
+++ src/components/upload/progressList/ProgressList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProgressItems from './ProgressItems'
+
+const ProgressList = () => {
+  return (
+    <ImagesList rowHeigth={200} cols={4}>
+    <ProgressItems />
+      
+    </ImagesList>
+  )
+}
+
+export default ProgressList
